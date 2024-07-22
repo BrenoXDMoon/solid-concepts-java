3e59211724e3ef875d6189f17e54b448fbaca436
brenoxdmoon <brenogabriel19@gmail.com>
2024-07-22 07:55:48 -0300

    Initial commit

---
+++ src/main/java/br/com/brenoxdmoon/solidconcepts/SolidConceptsApplication.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.solidconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidConceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidConceptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/brenoxdmoon/solidconcepts/SolidConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.solidconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
